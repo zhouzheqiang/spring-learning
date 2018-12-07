59ae2a91bd1fe560941691a5e77aaeb7b8f08696
zhouzheqiang <zhouzheqiang66@hotmail.com>
2018-12-07 14:24:54 +0800

    implement enhancements:     hystrix circuit breaker & fallback method

--- consumer/src/main/java/com/zzq/learning/ConsumerApplication.java
+++ consumer/src/main/java/com/zzq/learning/ConsumerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 /**
  * Hello world!
@@ -15,7 +14,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableCircuitBreaker
-@EnableHystrixDashboard
+//@EnableHystrixDashboard
 public class ConsumerApplication
 {
     public static void main( String[] args )
--- consumer/src/main/java/com/zzq/learning/controller/ConsumerController.java
+++ consumer/src/main/java/com/zzq/learning/controller/ConsumerController.java
@@ -1,5 +1,6 @@
 package com.zzq.learning.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.zzq.learning.api.ConsumerApi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +20,15 @@ public class ConsumerController {
     @GetMapping
     public String consumer(@RequestParam String name) {
         return consumerApi.hello(name);
+    }
+
+    @HystrixCommand(fallbackMethod = "getDefaultString")
+    @GetMapping("/turbineTest")
+    public String turbineTest(@RequestParam String name) {
+        return consumerApi.hello(name);
+    }
+
+    private String getDefaultString(String name) {
+        return "ERROR";
     }
 }
